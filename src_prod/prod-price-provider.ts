63e2dd1868ac14ff6cc394409c7f846e03de43ae
Big Baby <92488786+bbftso@users.noreply.github.com>
2021-10-25 20:15:11 +0000

    Update prod-price-provider.ts

--- src_prod/prod-price-provider.ts
+++ src_prod/prod-price-provider.ts
@@ -355,7 +355,7 @@ async function main() {
     // Need a bit of buffer to let the other function calls return
     // Should be based on when others submit their prices to make sure we're as close as possible to them
     // submitBuffer = submitBufferBase + mean(submitTimes) + submitBufferStd*std(submitTimes)
-    var submitBuffer = 18;              // Initial buffer for how many seconds before end of epoch we should start submission
+    var submitBuffer = 22;              // Initial buffer for how many seconds before end of epoch we should start submission
     var submitTimes: Number[] = [];     // Record recent times to measure how much buffer we need
     var submitBufferStd = 3;            // How many stds (normal)
     // var submitBufferDecay = 0.999;      // Decay factor on each loop
@@ -505,8 +505,8 @@ async function main() {
         if (nSubmitTimes > submitBufferBurnIn) {
             // new submitBuffer in seconds
             submitBuffer = submitBufferBase + submitMean + submitBufferStd*submitStd;
-            if(submitBuffer < 18)
-                submitBuffer = 18;
+            if(submitBuffer < 22)
+                submitBuffer = 22;
         }
         console.log(`   New:  ${submitBuffer}`);
         console.log(`   Mean: ${submitMean}`);
